ad021212d13d2a82af35a6cf4701fb34e825a5fa
rainmanp7 <muslimsoap@gmail.com>
2018-09-14 08:57:31 +0800

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+var api = 'http://explorer.phfirecoin.com:42081';
+var blockTargetInterval = 240;
+var coinUnits = 100000000;
+var symbol = 'phfx';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "phfx": [
+		["pool.phfirecoin.com", "http://pool.phfirecoin.com:42081"]
+    ]
+};
